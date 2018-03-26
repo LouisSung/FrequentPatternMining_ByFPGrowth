115871da621b73ca998b66af81897ca5f5236626
LouisSung <LouisSung@ls-mbpr.local>
2018-03-26 15:48:38 +0800

    Initial Commit

---
+++ HW2_Frequent_Patterns_FPGrowth/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW2_Frequent_Patterns_FPGrowth
+//
+//  Created by LouisSung on 2018/3/26.
+//  Copyright © 2018年 LS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
